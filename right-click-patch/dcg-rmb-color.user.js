32ed2e89a89351d8a3bcf337d687235c2017837b
Mario D Flores <43660822+SlimRunner@users.noreply.github.com>
2020-08-23 23:53:55 -0700

    rmb color refactor

--- right-click-patch/dcg-rmb-color.user.js
+++ right-click-patch/dcg-rmb-color.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name     	DesmosColorRightClick
 // @namespace	slidav.Desmos
-// @version  	1.0.6
+// @version  	1.1.0
 // @author		SlimRunner (David Flores)
 // @description	Overrides context menu for color bubble
 // @grant    	none
@@ -19,9 +19,11 @@
 	var tm_win;
 	var tm_doc;
 	
+	// initializes the event listeners
 	function initListeners () {
 		let showContextMenu = true;
 		
+		// cancels standard context menu
 		tm_doc.addEventListener("contextmenu", (e) => {
 			if (!showContextMenu) {
 				showContextMenu = true;
@@ -29,41 +31,81 @@
 			}
 		});
 		
+		// triggers the color menu and sets flag to cancel standard context menu
 		tm_win.addEventListener('mousedown', (e) => {
-			if (
-				e.button === 2 &&
-				typeof e.target.classList === 'object' &&
-				typeof e.target.className === 'string' &&
-				(e.target.classList.contains('dcg-layered-icon') ||
-				e.target.classList.contains('dcg-circular-icon'))
-			) {
-				const ICON_DICTIONARY = `boxplot cross distribution dotplot-cross dotplot-default dotplot-open histogram lines-solid move move-horizontal move-vertical open parametric-dashed parametric-dotted parametric-filled parametric-solid point points polygon-dashed polygon-dotted polygon-filled polygon-solid shaded-inequality-dash`;
+			if (e.button === 2) {
+				let tag = e.target.tagName.toLowerCase();
+				let container = null;
+				let ariaLabel = '';
 				
-				// isolate icon name using regex
-				let targetName = e.target.className.match(/(?<=dcg-icon-)[a-z\-]+/im);
-				
-				if (
-					ICON_DICTIONARY.search(targetName) !== -1
+				if ( // hidden color bubble of expressions or images
+					tag === 'span' &&
+					(container = seekParent(e.target, 2)) !== null &&
+					'classList' in container &&
+					container.classList.contains('dcg-expression-icon-container') &&
+					(ariaLabel = seekParent(e.target, 1).getAttribute("aria-label")) &&
+					(
+						ariaLabel.search('Expression') !== -1 ||
+						ariaLabel.search('Image') !== -1
+					)
 				) {
-					// when the bubble is visible
 					showContextMenu = false;
-					Desmos.$(e.target.parentElement.parentElement).trigger('dcg-longhold');
+					Desmos.$(seekParent(e.target, 1)).trigger('dcg-longhold');
 					
-				} else if (
-					e.target.className.search('dcg-do-not-blur') !== -1 &&
-					e.target.className.search('dcg-hidden') !== -1 &&
-					e.target.parentElement.parentElement.parentElement.parentElement.className.search('dcg-expressionfolder') === -1
+				} else if ( // shown color bubble of expressions
+					tag === 'i' &&
+					(container = seekParent(e.target, 3)) !== null &&
+					'classList' in container &&
+					container.classList.contains('dcg-expression-icon-container') &&
+					(ariaLabel = seekParent(e.target, 2).getAttribute("aria-label")) &&
+					(
+						ariaLabel.search('Expression') !== -1 ||
+						ariaLabel.search('Image') !== -1
+					)
 				) {
-					// when the bubble is hidden
 					showContextMenu = false;
-					Desmos.$(e.target.parentElement).trigger('dcg-longhold');
+					Desmos.$(seekParent(e.target, 2)).trigger('dcg-longhold');
+					
+				} else if ( // hidden color bubble of table columns
+					tag === 'span' &&
+					(container = seekParent(e.target, 2)) !== null &&
+					'classList' in container &&
+					container.classList.contains('dcg-table-icon-container')
+				) {
+					showContextMenu = false;
+					Desmos.$(seekParent(e.target, 1)).trigger('dcg-longhold');
+					
+					
+				} else if ( // shown color bubble of table columns
+					tag === 'i' &&
+					(container = seekParent(e.target, 3)) !== null &&
+					'classList' in container &&
+					container.classList.contains('dcg-table-icon-container')
+				) {
+					showContextMenu = false;
+					Desmos.$(seekParent(e.target, 2)).trigger('dcg-longhold');
 					
 				}
-				
 			}
 		});
 	}
 	
+	// traverse the DOM tree to find parentElements iteratively
+	function seekParent(src, level) {
+		if (level <= 0) return src;
+		
+		for (var i = 0; i < level; ++i) {
+			if (src != null) {
+				src = src.parentElement;
+			} else {
+				return null;
+			}
+		}
+		
+		return src;
+	}
+	
+	// initializes the script
 	(function loadCheck () {
 		
 		if (typeof loadCheck.attempts === 'undefined') {
